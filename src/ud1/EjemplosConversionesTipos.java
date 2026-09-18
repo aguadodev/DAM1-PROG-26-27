b3b5d0cbb5b1d90b3307747343e25465b821d513
aguadodev <aguado@edu.xunta.es>
2026-09-18 10:40:47 +0200

    ConversionTipos

---
+++ src/ud1/EjemplosConversionesTipos.java
@@ -0,0 +1,18 @@
+package ud1;
+
+public class EjemplosConversionesTipos {
+    public static void main(String[] args) {
+        double a = 56;
+        System.out.println(a);
+
+        int b = (int) (5.6 + 4.5);
+        System.out.println(b);
+
+        float c = 37453872452438l;
+        System.out.println(c);
+
+        double num = Double.parseDouble("345345.35lefkssgh");
+        System.out.println(num + 5);
+
+    }
+}
